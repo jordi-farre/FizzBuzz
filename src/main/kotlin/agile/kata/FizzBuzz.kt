1555a96d11aa27b81df98a0496e1dc7646d8973b
Jordi Farre <jordi.farre@mango.com>
2019-10-07 16:43:09 +0200

    Implement fizzbuzz for number 15

--- src/main/kotlin/agile/kata/FizzBuzz.kt
+++ src/main/kotlin/agile/kata/FizzBuzz.kt
@@ -2,6 +2,10 @@ package agile.kata
 
 class FizzBuzz {
 
-    fun stringFrom(number: Int): String = if (number % 3 == 0) "Fizz" else if (number % 5 == 0) "Buzz" else number.toString()
+    fun stringFrom(number: Int): String =
+        if (number % 3 == 0 && number % 5 == 0) "FizzBuzz"
+        else if (number % 3 == 0) "Fizz"
+        else if (number % 5 == 0) "Buzz"
+        else number.toString()
 
 }
