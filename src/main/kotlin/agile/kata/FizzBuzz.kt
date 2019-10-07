54e1be5fd051a69242a30772409ea360bbef7e87
Jordi Farre <jordi.farre@mango.com>
2019-10-07 17:00:01 +0200

    Replace if/else by when clause

--- src/main/kotlin/agile/kata/FizzBuzz.kt
+++ src/main/kotlin/agile/kata/FizzBuzz.kt
@@ -5,9 +5,11 @@ class FizzBuzz {
     private fun Int.divisibleBy(number: Int): Boolean = this % number == 0
 
     fun stringFrom(number: Int): String =
-        if (number.divisibleBy(3) && number.divisibleBy(5)) "FizzBuzz"
-        else if (number.divisibleBy(3)) "Fizz"
-        else if (number.divisibleBy(5)) "Buzz"
-        else number.toString()
+        when {
+            number.divisibleBy(3) && number.divisibleBy(5) -> "FizzBuzz"
+            number.divisibleBy(3) -> "Fizz"
+            number.divisibleBy(5) -> "Buzz"
+            else -> number.toString()
+        }
 
 }
