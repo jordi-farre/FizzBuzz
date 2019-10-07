db35f6a4769242cf7516b24ef9e65301ae05415d
Jordi Farre <jordi.farre@mango.com>
2019-10-07 16:57:53 +0200

    Remove hamcrest is and use directly equalsto

--- src/test/kotlin/agile/kata/FizzBuzzShould.kt
+++ src/test/kotlin/agile/kata/FizzBuzzShould.kt
@@ -1,7 +1,6 @@
 package agile.kata
 
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -13,25 +12,25 @@ class FizzBuzzShould {
     @ParameterizedTest
     @ValueSource(ints = [1, 2, 4])
     fun `return string representing the number for numbers not divisible by 3 or 5`(number: Int) {
-        assertThat(fizzBuzz.stringFrom(number), `is`(equalTo(number.toString())))
+        assertThat(fizzBuzz.stringFrom(number), (equalTo(number.toString())))
     }
 
     @ParameterizedTest
     @ValueSource(ints = [3, 12, 24])
     fun `return Fizz string for numbers divisible by 3`(number: Int) {
-        assertThat(fizzBuzz.stringFrom(number), `is`(equalTo("Fizz")))
+        assertThat(fizzBuzz.stringFrom(number), (equalTo("Fizz")))
     }
 
     @ParameterizedTest
     @ValueSource(ints = [5, 25, 55])
     fun `return Buzz string for numbers divisible by 5`(number: Int) {
-        assertThat(fizzBuzz.stringFrom(number), `is`(equalTo("Buzz")))
+        assertThat(fizzBuzz.stringFrom(number), (equalTo("Buzz")))
     }
 
     @ParameterizedTest
     @ValueSource(ints = [15, 30, 60, 150])
     fun `return Buzz string for numbers divisible by 3 and 5`(number: Int) {
-        assertThat(fizzBuzz.stringFrom(number), `is`(equalTo("FizzBuzz")))
+        assertThat(fizzBuzz.stringFrom(number), (equalTo("FizzBuzz")))
     }
 
 }
